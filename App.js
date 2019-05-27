7698c13daffb93401f0e82627b15b514bd2e0a4c
Alexis Hevia <hevia.alexis@gmail.com>
2019-05-26 20:45:04 -0500

    import (very poorly) mdo-core

--- App.js
+++ App.js
@@ -1,5 +1,17 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import key from "weak-key";
+
+// TODO: for some reason, the following code does not work:
+// ```
+// import MDo from "@mdo-org/mdo-core/lib/strings"
+// ```
+// So, I'm currently doing the following ugly hack:
+//   1. on package.json:
+//     "prestart": "cp ./node_modules/@mdo-org/mdo-core/lib/strings/index.js ./mdo-core.js"
+//   2. importing from ./mdo-core.js
+import MDo from "./mdo-core";
+
 import Block from "./src/components/Block";
 
 const styles = StyleSheet.create({
@@ -11,17 +23,47 @@ const styles = StyleSheet.create({
   }
 });
 
-const blocks = [{ text: "hello world" }];
+const initialText = `
+Hello MDo.
 
-const renderBlock = block => <Block key={block.text} block={block} />;
+# Work
+- [ ] Spreadsheet all the things
+  @start tomorrow
 
-/* eslint react/prefer-stateless-function:[0] */
+# Home
+Tasks I plan to work on when I'm done with work.
+
+- [X] Take out trash
+- [ ] Fix the basement light
+  Not sure what's going on.
+
+  Faulty light bulb?
+`.trim();
+
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blocks: []
+    };
+  }
+
+  componentDidMount() {
+    this.parseFile();
+  }
+
+  parseFile() {
+    MDo.parse(initialText).then(blocks => this.setState({ blocks }));
+  }
+
+  /* eslint react/destructuring-assignment:[0] */
   render() {
     return (
       <View style={styles.container}>
         <Text>MDo Native</Text>
-        {blocks.map(renderBlock)}
+        {this.state.blocks.map(block => (
+          <Block key={key(block)} block={block} />
+        ))}
       </View>
     );
   }
--- src/components/Block/index.js
+++ src/components/Block/index.js
@@ -2,11 +2,26 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Text } from "react-native";
 
-const Block = ({ block }) => <Text>- [ ] {block.text}</Text>;
+const blockTypeStringMap = new Map([
+  ["COMPLETE_TASK", "- [X]"],
+  ["INCOMPLETE_TASK", "- [ ]"],
+  ["COMMENT", "#"],
+  ["PADDING", ""]
+]);
+
+const Block = ({ block }) => {
+  const typeAsString = blockTypeStringMap.get(block.type) || "";
+  return (
+    <Text>
+      {typeAsString} {block.text}
+    </Text>
+  );
+};
 
 Block.propTypes = {
   block: PropTypes.shape({
-    text: PropTypes.string
+    type: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired
   }).isRequired
 };
 
